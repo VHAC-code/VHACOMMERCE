fdcb2dc62c357084f13e90f1a0840e568f6044cc
VHAC code <vachaspati1997@gmail.com>
2024-11-07 18:55:07 +0530

    Updated store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,88 @@
-// import { configureStore } from "@reduxjs/toolkit"; // Import configureStore from Redux Toolkit
-import { createStore, combineReducers, applyMiddleware } from "redux"; //createStore removed from here
-import { thunk } from "redux-thunk"; // Correct import for redux-thunk
-import { composeWithDevTools } from "redux-devtools-extension";
+// // import { configureStore } from "@reduxjs/toolkit"; // Import configureStore from Redux Toolkit
+// import { createStore, combineReducers, applyMiddleware } from "redux"; //createStore removed from here
+// import { thunk } from "redux-thunk"; // Correct import for redux-thunk
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import {
+//   newProductReducer,
+//   newReviewReducer,
+//   productDetailsReducer,
+//   productReducer,
+//   productReviewsReducer,
+//   productsReducer,
+//   reviewReducer,
+// } from "./reducers/productReducer";
+
+// import {
+//   allUsersReducer,
+//   forgotPasswordReducer,
+//   profileReducer,
+//   userDetailsReducer,
+//   userReducer,
+// } from "./reducers/userReducer";
+
+// import { cartReducer } from "./reducers/cartReducer";
+// import {
+//   allOrdersReducer,
+//   myOrdersReducer,
+//   newOrderReducer,
+//   orderDetailsReducer,
+//   orderReducer,
+// } from "./reducers/orderReducer";
+
+// const reducer = combineReducers({
+//   products: productsReducer,
+//   productDetails: productDetailsReducer,
+//   user: userReducer,
+//   profile: profileReducer,
+//   forgotPassword: forgotPasswordReducer,
+//   cart: cartReducer,
+//   newOrder: newOrderReducer,
+//   myOrders: myOrdersReducer,
+//   orderDetails: orderDetailsReducer,
+//   newReview: newReviewReducer,
+//   newProduct: newProductReducer,
+//   product: productReducer,
+//   allOrders: allOrdersReducer,
+//   order: orderReducer,
+//   allUsers: allUsersReducer,
+//   userDetails: userDetailsReducer,
+//   productReviews: productReviewsReducer,
+//   review: reviewReducer,
+// });
+
+// let initialState = {
+//   cart: {
+//     cartItems: localStorage.getItem("cartItems")
+//       ? JSON.parse(localStorage.getItem("cartItems"))
+//       : [],
+//     shippingInfo: localStorage.getItem("shippingInfo")
+//       ? JSON.parse(localStorage.getItem("shippingInfo"))
+//       : {},
+//   },
+// };
+
+// const middleware = [thunk]; // Applying redux-thunk middleware
+
+// // // Create store with dev tools and middleware
+// const store = createStore(
+//   reducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware)) // Apply middleware correctly
+// );
+
+// // const store = configureStore({
+// //   reducer,
+// //   preloadedState: initialState, // Provide initial state
+// //   // Middleware setup (redux-thunk is included by default in Redux Toolkit)
+// // });
+
+// export default store;
+
+//2nd way
+
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import thunk from "redux-thunk";
 import {
   newProductReducer,
   newReviewReducer,
@@ -29,6 +110,7 @@ import {
   orderReducer,
 } from "./reducers/orderReducer";
 
+// Combine all reducers
 const reducer = combineReducers({
   products: productsReducer,
   productDetails: productDetailsReducer,
@@ -50,7 +132,8 @@ const reducer = combineReducers({
   review: reviewReducer,
 });
 
-let initialState = {
+// Define initial state with cart items
+const initialState = {
   cart: {
     cartItems: localStorage.getItem("cartItems")
       ? JSON.parse(localStorage.getItem("cartItems"))
@@ -61,19 +144,11 @@ let initialState = {
   },
 };
 
-const middleware = [thunk]; // Applying redux-thunk middleware
-
-// // Create store with dev tools and middleware
-const store = createStore(
+// Configure store using configureStore from Redux Toolkit
+const store = configureStore({
   reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware)) // Apply middleware correctly
-);
-
-// const store = configureStore({
-//   reducer,
-//   preloadedState: initialState, // Provide initial state
-//   // Middleware setup (redux-thunk is included by default in Redux Toolkit)
-// });
+  preloadedState: initialState,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
 
 export default store;
