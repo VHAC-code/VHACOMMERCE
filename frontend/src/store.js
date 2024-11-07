f40dafc11b5adb853f157f6f17e2e827badbbac1
VHAC code <vachaspati1997@gmail.com>
2024-11-07 15:05:55 +0530

    Updated store.js thunk code

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk"; // Correct import for redux-thunk
+import { thunk } from "redux-thunk"; // Correct import for redux-thunk
 import { composeWithDevTools } from "redux-devtools-extension";
 import {
   newProductReducer,
